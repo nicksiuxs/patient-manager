f34a44ddfa606c713e847f6f1a3080de38c41804
Nicolas <nicksiuxs@gmail.com>
2021-01-24 22:36:07 -0500

    ADD:  form component state was added

--- src/components/Formulario/Form.js
+++ src/components/Formulario/Form.js
@@ -1,4 +1,28 @@
+import {useState} from 'react';
+
 const Form = () => {
+
+    // Creating the state of appointment
+    const[appointment, updateAppointment] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: '',
+    });
+
+    // Function that excute when the user type in an input
+    const handleChangeInput = (event)=>{
+        
+        updateAppointment({
+            ...appointment,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    // extract the values of the json
+    const {mascota,propietario,fecha,hora,sintomas} = appointment;
+
     return ( 
     <div>
         <h2>Crear Cita</h2>
@@ -9,6 +33,8 @@ const Form = () => {
                 name="mascota"
                 className="u-full-width"
                 placeholder="Nombre de la mascota"
+                onChange={handleChangeInput}
+                value={mascota}
             />
             <label>Nombre Dueño</label>
             <input
@@ -16,23 +42,31 @@ const Form = () => {
                 name="propietario"
                 className="u-full-width"
                 placeholder="Nombre de del dueño de la mascota"
+                onChange={handleChangeInput}
+                value={propietario}
             />
             <label>Fecha</label>
             <input
                 type="date"
                 name="fecha"
                 className="u-full-width"
+                onChange={handleChangeInput}
+                value={fecha}
             />
             <label>Hora</label>
             <input
                 type="time"
                 name="hora"
                 className="u-full-width"
+                onChange={handleChangeInput}
+                value={hora}
             />
             <label>Sintomas</label>
             <textarea
                 className="u-full-width"
                 name="sintomas"
+                onChange={handleChangeInput}
+                value={sintomas}
             >
 
             </textarea>
